434b12b026d2a7ed4628cdd94b6193fb07ea5dee
Per Abich <per.abich@gmail.com>
2017-07-24 06:27:33 +0100

    Even more improvements

--- roles/consul/files/consul-destroy-install.sh
+++ roles/consul/files/consul-destroy-install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -e
+#set -e
 
+sudo systemctl stop vault.service
 sudo systemctl stop consul.service && \
 sudo rm -rf /usr/local/bin/consul && \
 sudo rm -rf /usr/local/consul && \
 sudo rm -rf /var/lib/consul && \
-sudo rm -rf /etc/consul
+sudo rm -rf /etc/consul &&
+exit 0
+exit 1
 
